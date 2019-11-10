eee1ecf417cc1eca5faf06d7a9d60cbcf85c533c
dowon <48977911+devdw98@users.noreply.github.com>
2019-11-10 13:48:03 +0900

    Python Algorithm_basic - Code_Festival

---
+++ Python/Algorithm/Code_Festival/jeju_13.py
@@ -0,0 +1,5 @@
+num = (int)(input("enter number: "))
+
+planet = ['수성', '금성', '지구', '화성', '목성','토성','천왕성','해왕성']
+
+print(planet[num-1])
---
+++ Python/Algorithm/Code_Festival/jeju_15.py
@@ -0,0 +1,3 @@
+name = input("enter your name: ")
+
+print("안녕하세요. 저는 "+name+"입니다.")
