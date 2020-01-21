30e6db0061d65a0b794e384d5851d64984d46c2e
dw <devdw98@gmail.com>
2020-01-21 17:12:43 +0900

    CRUD 구현

---
+++ Spring/board0119/src/main/java/org/dw0119/controller/BoardController.java
@@ -0,0 +1,61 @@
+package org.dw0119.controller;
+
+import org.dw0119.domain.BoardVO;
+import org.dw0119.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register");
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list"; //spring mvc가 내부적으로 response.sendRedirect() 처리를 해줌
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: "+board);
+		
+		if(service.modify(board))
+			rttr.addFlashAttribute("result", "success");
+		return "redirect: /board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove .."+bno);
+		if(service.delete(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect: /board/list";
+	}
+	
+}
--- Spring/board0119/src/main/java/org/dw0119/mapper/BoardMapper.java
+++ Spring/board0119/src/main/java/org/dw0119/mapper/BoardMapper.java
@@ -11,6 +11,7 @@ public interface BoardMapper {
 	public List<BoardVO> getList();
 	
 	public void insert(BoardVO board);
+	
 	public void insertSelectKey(BoardVO board);
 
 	public BoardVO read(Long bno);
