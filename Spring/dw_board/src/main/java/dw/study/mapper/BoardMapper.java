050c129b813f0af19bf524d9d14a285e6b3893f4
devdw98 <devdw98@gmail.com>
2020-02-18 01:20:42 +0900

    create resource mapper

--- Spring/dw_board/src/main/java/dw/study/mapper/BoardMapper.java
+++ Spring/dw_board/src/main/java/dw/study/mapper/BoardMapper.java
@@ -8,7 +8,7 @@ import dw.study.domain.BoardVO;
 
 public interface BoardMapper { //2. SQL 작업해 줄 Mapper Interface 작성
 	
-	@Select("select * from tbl_board where bno > 0")
+//	@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
 
 }
--- Spring/dw_board/src/test/java/dw/study/mapper/BoardMapperTests.java
+++ Spring/dw_board/src/test/java/dw/study/mapper/BoardMapperTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.mysql.cj.log.Log;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
