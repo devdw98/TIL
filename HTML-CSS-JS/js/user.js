6207264b7c92182918c293a65f0c2f951ed079c4
dw <devdw98@gmail.com>
2020-04-28 15:30:58 +0900

    js 기초 강의 듣기

---
+++ HTML-CSS-JS/js/user.js
@@ -0,0 +1,92 @@
+var user = [
+    {
+      "_id": "5ea7c8c7026391d8205cc228",
+      "age": 35,
+      "eyeColor": "green",
+      "name": "Mitchell Cannon",
+      "gender": "male",
+      "email": "mitchellcannon@comveyor.com",
+      "phone": "+1 (815) 400-3663"
+    },
+    {
+      "_id": "5ea7c8c7e208091a87313006",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Alvarez Ryan",
+      "gender": "male",
+      "email": "alvarezryan@comveyor.com",
+      "phone": "+1 (880) 463-3895"
+    },
+    {
+      "_id": "5ea7c8c7c369925a3fad6889",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Michelle Dennis",
+      "gender": "female",
+      "email": "michelledennis@comveyor.com",
+      "phone": "+1 (947) 461-3147"
+    },
+    {
+      "_id": "5ea7c8c78aba8d181b81c34d",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Frankie Barry",
+      "gender": "female",
+      "email": "frankiebarry@comveyor.com",
+      "phone": "+1 (924) 554-3115"
+    },
+    {
+      "_id": "5ea7c8c723651b6079eaf66b",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Stacey Clements",
+      "gender": "female",
+      "email": "staceyclements@comveyor.com",
+      "phone": "+1 (813) 591-3783"
+    },
+    {
+      "_id": "5ea7c8c7f911ab6c659ee6f8",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Wheeler Nieves",
+      "gender": "male",
+      "email": "wheelernieves@comveyor.com",
+      "phone": "+1 (825) 597-3076"
+    },
+    {
+      "_id": "5ea7c8c7c09faae5908c0914",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Megan Pace",
+      "gender": "female",
+      "email": "meganpace@comveyor.com",
+      "phone": "+1 (802) 591-3362"
+    },
+    {
+      "_id": "5ea7c8c7f6918e0e2b34e4a1",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Jacklyn Moody",
+      "gender": "female",
+      "email": "jacklynmoody@comveyor.com",
+      "phone": "+1 (892) 479-3249"
+    },
+    {
+      "_id": "5ea7c8c7512a170b498de903",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Lopez Herring",
+      "gender": "male",
+      "email": "lopezherring@comveyor.com",
+      "phone": "+1 (873) 404-2714"
+    },
+    {
+      "_id": "5ea7c8c7c9b3e3b5b61e995e",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Josefa Burt",
+      "gender": "female",
+      "email": "josefaburt@comveyor.com",
+      "phone": "+1 (950) 449-2370"
+    }
+  ]
