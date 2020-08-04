d9b895432e522f5c300270f107ccda689f040a32
dw <devdw98@gmail.com>
2020-08-04 15:33:26 +0900

    순수jdbc + solid 개방-폐쇄 원칙

--- spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource ds;
+    @Autowired
+    public SpringConfig(DataSource ds){
+        this.ds = ds;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -15,6 +25,7 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(ds);
     }
 }
