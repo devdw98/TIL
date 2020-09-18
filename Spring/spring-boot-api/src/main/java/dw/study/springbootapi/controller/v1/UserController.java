e7dd3d94a4a907bb92958689ae49baf4ab3255cd
dowon <devdw98@gmail.com>
2020-09-18 23:29:11 +0900

    남은거

--- Spring/spring-boot-api/src/main/java/dw/study/springbootapi/controller/v1/UserController.java
+++ Spring/spring-boot-api/src/main/java/dw/study/springbootapi/controller/v1/UserController.java
@@ -1,29 +1,60 @@
 package dw.study.springbootapi.controller.v1;
 
 import dw.study.springbootapi.entity.User;
+import dw.study.springbootapi.model.response.CommonResult;
+import dw.study.springbootapi.model.response.ListResult;
+import dw.study.springbootapi.model.response.SingleResult;
 import dw.study.springbootapi.repository.UserRepository;
+import dw.study.springbootapi.service.ResponseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"}) //최상단 타이틀 영역에 표시될 값 세팅
 @RequiredArgsConstructor //final로 선언된 객체에 대해 Constructor Injection 수행
 @RestController
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserRepository userRepository;
+    private final ResponseService responseService;
 
-    @GetMapping("/user")
-    public List<User> findAllUser(){
-        return userRepository.findAll();
+    @ApiOperation(value = "회원 리스트 조회", notes="모든 회원 조회") // 제목과 설명
+    @GetMapping("/users")
+    public ListResult<User> findAllUser(){
+        return responseService.getListResult(userRepository.findAll());
     }
 
+    @ApiOperation(value = "회원 조회", notes = "회원을 조회한다")
+    @GetMapping("/user/{id}")
+    public SingleResult<User> findUserById(@ApiParam(value="회원번호", required = true) @RequestParam long id){
+        return responseService.getSingleResult(userRepository.findById(id).orElse(null));
+    }
+
+    @ApiOperation(value = "회원 입력", notes = "회원 입력한다")
     @PostMapping("/user")
-    public User save(){
-        User user = User.builder().uid("test@dwtest.com").name("test1").build();
-        return userRepository.save(user);
+    public SingleResult<User> save(@ApiParam(value="회원아이디", required = true) @RequestParam String uid, //파라미터에 대한 설명
+                                   @ApiParam(value="회원이름",required = true) @RequestParam String name){
+        User user = User.builder().uid(uid).name(name).build();
+        return responseService.getSingleResult(userRepository.save(user));
+    }
+
+    @ApiOperation(value = "회원 수정", notes = "회원 정보를 수정한다")
+    @PutMapping("/user")
+    public SingleResult<User> modify(@ApiParam(value="회원번호", required = true) @RequestParam long id,
+                                     @ApiParam(value="회원아이디", required = true) @RequestParam String uid,
+                                     @ApiParam(value="회원이름",required = true) @RequestParam String name){
+        User user = User.builder().id(id).uid(uid).name(name).build();
+        return responseService.getSingleResult(userRepository.save(user));
+    }
+
+    @ApiOperation(value = "회원 삭제", notes = "회원 정보를 삭제한다")
+    @DeleteMapping("/user/{id}")
+    public CommonResult delete(@ApiParam(value="회원번호", required = true) @PathVariable long id){
+        userRepository.deleteById(id);
+        return responseService.getSuccessResult();
     }
 }
